77b7cd54f6b2b720eca3cce3665eaede7d897268
aphawkins <aph828@gmail.com>
2019-01-05 23:05:19 +0000

    SpecFlow-MSTest to xUnit (#7)

--- src/Useful.UnitTests/README.md
+++
@@ -1,4 +0,0 @@
-### Useful.UnitTests Library
-
-* Class Library (.NET Framework 4.6.2)
-* Unit tests for the Useful.dll assembly.
---
+++ src/Useful.UnitTests/Security/Cryptography/ROT13CipherTests.cs
@@ -0,0 +1,46 @@
+using Moq;
+using System;
+using Useful.Security.Cryptography;
+using Xunit;
+
+namespace Useful.UnitTests
+{
+    public class ROT13CipherTests : IDisposable
+    {
+        private ICipher _cipher;
+
+        public ROT13CipherTests()
+        {
+            _cipher = new ROT13Cipher();
+        }
+
+        [Fact]
+        public void Name()
+        {
+            Assert.Equal("ROT13", _cipher.CipherName);
+        }
+
+        [Theory]
+        [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "NOPQRSTUVWXYZABCDEFGHIJKLM")]
+        [InlineData("abcdefghijklmnopqrstuvwxyz", "nopqrstuvwxyzabcdefghijklm")]
+        [InlineData(">?@ [\\]", ">?@ [\\]")]
+        public void Encrypt(string plaintext, string ciphertext)
+        {
+            Assert.Equal(ciphertext, _cipher.Encrypt(plaintext));
+        }
+
+        [Theory]
+        [InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "NOPQRSTUVWXYZABCDEFGHIJKLM")]
+        [InlineData("abcdefghijklmnopqrstuvwxyz", "nopqrstuvwxyzabcdefghijklm")]
+        [InlineData(">?@ [\\]", ">?@ [\\]")]
+        public void Decrypt(string plaintext, string ciphertext)
+        {
+            Assert.Equal(plaintext, _cipher.Decrypt(ciphertext));
+        }
+
+        public void Dispose()
+        {
+            _cipher = null;
+        }
+    }
+}
