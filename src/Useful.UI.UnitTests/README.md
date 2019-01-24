e306e18369f69d91f94ab22c23e404fb54225177
aphawkins <aph828@gmail.com>
2019-01-24 22:42:45 +0000

    SpecFlow-MSTest to xUnit (#7)

--- src/Useful.UI.UnitTests/README.md
+++
@@ -1,4 +0,0 @@
-### Useful.UI.UnitTests Library
-
-* Class Library (.NET Framework 4.6.2)
-* Unit tests for the Useful.UI.dll assembly.
---
+++ src/Useful.UI.UnitTests/ViewModels/CipherViewModelTests.cs
@@ -0,0 +1,140 @@
+using Moq;
+using System.Collections.Generic;
+using Useful.Security.Cryptography;
+using Useful.UI.ViewModels;
+using Xunit;
+
+namespace Useful.UI.UnitTests.ViewModels.xUnit
+{
+    public class CipherViewModelTests
+    {
+        private CipherViewModel _viewModel;
+        private Mock<ICipherRepository> _moqRepository;
+        private Mock<ICipher> _moqCipher;
+        private CipherService _cipherService;
+
+        public CipherViewModelTests()
+        {
+            _moqCipher = new Mock<ICipher>();
+            _moqCipher.Setup(x => x.CipherName).Returns("MoqCipher");
+            _moqCipher.Setup(x => x.Encrypt(It.IsAny<string>())).Returns("MoqCiphertext");
+            _moqCipher.Setup(x => x.Decrypt(It.IsAny<string>())).Returns("MoqPlaintext");
+            _moqRepository = new Mock<ICipherRepository>();
+            _moqRepository.Setup(x => x.Read()).Returns(() => new List<ICipher>() { _moqCipher.Object });
+            _cipherService = new CipherService(_moqRepository.Object);
+            _viewModel = new CipherViewModel(_cipherService);
+        }
+
+        [Fact]
+        public void Initialization()
+        {
+            // Assert.NotNull(viewModel.CurrentCipher);
+            //Assert.Equal("MoqCipher", viewModel.CurrentCipher.CipherName);
+            //Assert.NotNull(viewModel.EncryptCommand);
+            //viewModel.EncryptCommand.Execute(null);
+        }
+
+        [Fact]
+        public void SubscribedPlaintextChanged()
+        {
+            string propertyChanged;
+            _viewModel.PropertyChanged += (sender, e) => { propertyChanged = e.PropertyName; };
+            _viewModel.Plaintext = string.Empty;
+            propertyChanged = string.Empty;
+            _viewModel.Plaintext = "MoqPlaintext";
+            Assert.Equal("Plaintext", propertyChanged);
+        }
+
+        [Fact]
+        public void CipherViewModelPlaintext()
+        {
+            string propertyChanged;
+            _viewModel.PropertyChanged += (sender, e) => { propertyChanged = e.PropertyName; };
+            _viewModel.Plaintext = string.Empty;
+            propertyChanged = string.Empty;
+            _viewModel.Plaintext = "MoqPlaintext";
+            Assert.Equal("Plaintext", propertyChanged);
+        }
+
+        [Fact]
+        public void CipherViewModelPlaintextNoEventSubscription()
+        {
+            string propertyChanged;
+            _viewModel.Plaintext = string.Empty;
+            propertyChanged = string.Empty;
+            _viewModel.Plaintext = "MoqPlaintext";
+            Assert.Equal(string.Empty, propertyChanged);
+        }
+
+        [Fact]
+        public void CipherViewModelCiphertext()
+        {
+            string propertyChanged;
+            _viewModel.PropertyChanged += (sender, e) => { propertyChanged = e.PropertyName; };
+            _viewModel.Ciphertext = string.Empty;
+            propertyChanged = string.Empty;
+            _viewModel.Ciphertext = "MoqCiphertext";
+            Assert.Equal("Ciphertext", propertyChanged);
+        }
+
+        [Fact]
+        public void CipherViewModelCurrentCipher()
+        {
+            string propertyChanged;
+            _viewModel.PropertyChanged += (sender, e) => { propertyChanged = e.PropertyName; };
+            _viewModel.CurrentCipher = _moqCipher.Object;
+            _viewModel.CurrentCipher = null;
+            propertyChanged = string.Empty;
+            _viewModel.CurrentCipher = _moqCipher.Object;
+            Assert.Equal("CurrentCipher", propertyChanged);
+        }
+
+        [Fact]
+        public void CipherViewModelEncrypt()
+        {
+            _viewModel.Plaintext = "MoqPlaintext";
+            _viewModel.Encrypt();
+            Assert.Equal("MoqCiphertext", _viewModel.Ciphertext);
+        }
+
+        [Fact]
+        public void CipherViewModelEncryptCommand()
+        {
+            Assert.Equal("MoqPlaintext", _viewModel.Plaintext);
+            Assert.True(_viewModel.EncryptCommand.CanExecute(null));
+            _viewModel.EncryptCommand.Execute(null);
+            Assert.Equal("MoqCiphertext", _viewModel.Ciphertext);
+        }
+
+        [Fact]
+        public void CipherViewModelEncryptCommandNotExecutable()
+        {
+            Assert.Equal(string.Empty, _viewModel.Plaintext);
+            Assert.False(_viewModel.EncryptCommand.CanExecute(null));
+        }
+
+        [Fact]
+        public void CipherViewModelDecrypt()
+        {
+            _viewModel.Ciphertext = "MoqCiphertext";
+            _viewModel.Decrypt();
+            Assert.Equal("MoqPlaintext", _viewModel.Plaintext);
+        }
+
+        [Fact]
+        public void CipherViewModelDecryptCommand()
+        {
+            _viewModel.Ciphertext = "MoqCiphertext";
+            Assert.True(_viewModel.DecryptCommand.CanExecute(null));
+            _viewModel.DecryptCommand.Execute(null);
+            Assert.Equal("MoqPlaintext", _viewModel.Plaintext);
+        }
+
+        [Fact]
+        public void CipherViewModelDecryptCommandNotExecutable()
+        {
+            _viewModel.Ciphertext = string.Empty;
+            Assert.False(_viewModel.DecryptCommand.CanExecute(null));
+        }
+    }
+}
