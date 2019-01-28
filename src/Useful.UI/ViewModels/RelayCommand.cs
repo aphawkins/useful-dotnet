e35f9bc082da15a4488cabdb91b2ded29b285f74
aphawkins <aph828@gmail.com>
2019-01-28 22:15:01 +0000

    Codemaid clean (#12)

--- src/Useful.UI/ViewModels/RelayCommand.cs
+++ src/Useful.UI/ViewModels/RelayCommand.cs
@@ -29,6 +29,7 @@ namespace Useful.UI.ViewModels
         }
 
 #pragma warning disable CS0067
+
         /// <summary>
         /// Occurs when changes occur that affect whether or not the command should execute.
         /// </summary>
@@ -48,6 +49,7 @@ namespace Useful.UI.ViewModels
         }
 #else
         ;
+
 #endif
 #pragma warning restore CS0067
 
