19b1795eb72fafb77c37b592e09577d8d0ad591f
aphawkins <aph828@gmail.com>
2019-02-12 21:25:25 +0000

    Add Directory.Build.props (#20) + fix issues

--- src/Useful.UnitTests/Security/Cryptography/ReverseCipherTests.cs
+++ src/Useful.UnitTests/Security/Cryptography/ReverseCipherTests.cs
@@ -1,5 +1,5 @@
-// <copyright file="ReverseCipherTests.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+// <copyright file="ReverseCipherTests.cs" company="APH Software">
+// Copyright (c) Andrew Hawkins. All rights reserved.
 // </copyright>
 
 namespace Useful.UnitTests
