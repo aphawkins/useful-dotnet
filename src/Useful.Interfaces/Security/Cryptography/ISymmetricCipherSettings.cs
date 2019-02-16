4c51736f652e3cfc11a21b83bc94668c448ff7e2
aphawkins <aph828@gmail.com>
2019-02-16 21:51:34 +0000

    Implement Caesar SymmetricAlgorithm #22

---
+++ src/Useful.Interfaces/Security/Cryptography/ISymmetricCipherSettings.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="ISymmetricCipherSettings.cs" company="APH Software">
+// Copyright (c) Andrew Hawkins. All rights reserved.
+// </copyright>
+
+namespace Useful.Security.Cryptography
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Settings required to perform a symmetric cipher algorithm.
+    /// </summary>
+    public interface ISymmetricCipherSettings : ICipherSettings
+    {
+        /// <summary>
+        /// Gets the encryption Key.
+        /// </summary>
+        /// <value>The encryption Key.</value>
+        /// <returns>Encryption Key.</returns>
+        ICollection<byte> Key
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the Initialization Vector.
+        /// </summary>
+        /// <value>The Initialization Vector.</value>
+        /// <returns>Initialization Vector.</returns>
+        ICollection<byte> IV
+        {
+            get;
+        }
+    }
+}
