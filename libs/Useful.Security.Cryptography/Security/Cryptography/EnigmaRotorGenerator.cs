4687e5e55d0043d99befd1386d9264193b532ca4
aphawkins <aph828@gmail.com>
2020-11-25 22:39:55 +0000

    refactor

---
+++ libs/Useful.Security.Cryptography/Security/Cryptography/EnigmaRotorGenerator.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="EnigmaRotorGenerator.cs" company="APH Software">
+// Copyright (c) Andrew Hawkins. All rights reserved.
+// </copyright>
+
+namespace Useful.Security.Cryptography
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Enigma Reflector settings generator.
+    /// </summary>
+    internal class EnigmaRotorGenerator
+    {
+        public static EnigmaRotorSettings Generate()
+        {
+            Random rnd = new();
+            int nextRandomNumber;
+            EnigmaRotorSettings rotorSettings = new();
+
+            IList<EnigmaRotorPosition> availableRotorPositions = new List<EnigmaRotorPosition>()
+            {
+                EnigmaRotorPosition.Fastest,
+                EnigmaRotorPosition.Second,
+                EnigmaRotorPosition.Third,
+            };
+
+            IList<EnigmaRotorNumber> availableRotorNumbers;
+
+            foreach (EnigmaRotorPosition rotorPosition in availableRotorPositions)
+            {
+                availableRotorNumbers = new List<EnigmaRotorNumber>(rotorSettings.GetAvailableRotors());
+                nextRandomNumber = rnd.Next(0, availableRotorNumbers.Count);
+                rotorSettings[rotorPosition] = new(availableRotorNumbers[nextRandomNumber], new Random().Next(1, 26), 'A');
+            }
+
+            return rotorSettings;
+        }
+    }
+}
--- libs/Useful.Security.Cryptography/Security/Cryptography/EnigmaSettingsGenerator.cs
+++ libs/Useful.Security.Cryptography/Security/Cryptography/EnigmaSettingsGenerator.cs
@@ -5,7 +5,6 @@
 namespace Useful.Security.Cryptography
 {
     using System;
-    using System.Collections.Generic;
 
     /// <summary>
     /// Enigma key generator.
@@ -20,7 +19,7 @@ namespace Useful.Security.Cryptography
             reflectorNumber = reflector.ReflectorNumber;
 
             // Rotor Settings
-            IEnigmaRotorSettings rotorSettings = GetRandomRotorSettings();
+            IEnigmaRotorSettings rotorSettings = EnigmaRotorGenerator.Generate();
 
             // Plugboard
             IEnigmaPlugboard plugboard = EnigmaPlugboardGenerator.Generate();
@@ -37,46 +36,6 @@ namespace Useful.Security.Cryptography
             return settings;
         }
 
-        private static EnigmaRotor GetRandomRotor(EnigmaRotorNumber rotorNumber)
-        {
-            Random rnd = new();
-            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-            EnigmaRotor rotor = new(rotorNumber, rnd.Next(1, letters.Length), 'A');
-
-            return rotor;
-        }
-
-        private static char GetRandomRotorCurrentSetting()
-        {
-            Random rnd = new();
-            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            return letters[rnd.Next(0, letters.Length - 1)];
-        }
-
-        private static EnigmaRotorSettings GetRandomRotorSettings()
-        {
-            Random rnd = new();
-            int nextRandomNumber;
-            EnigmaRotorSettings rotorSettings = new();
-
-            IList<EnigmaRotorPosition> availableRotorPositions = new List<EnigmaRotorPosition>()
-            {
-                EnigmaRotorPosition.Fastest,
-                EnigmaRotorPosition.Second,
-                EnigmaRotorPosition.Third,
-            };
-
-            IList<EnigmaRotorNumber> availableRotorNumbers;
-
-            foreach (EnigmaRotorPosition rotorPosition in availableRotorPositions)
-            {
-                availableRotorNumbers = new List<EnigmaRotorNumber>(rotorSettings.GetAvailableRotors());
-                nextRandomNumber = rnd.Next(0, availableRotorNumbers.Count);
-                rotorSettings[rotorPosition] = GetRandomRotor(availableRotorNumbers[nextRandomNumber]);
-            }
-
-            return rotorSettings;
-        }
+        private static char GetRandomRotorCurrentSetting() => "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[new Random().Next(0, 25)];
     }
 }
