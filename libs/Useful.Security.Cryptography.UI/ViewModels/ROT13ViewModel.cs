b8886e21ac950b8110fcf247fcda3a6a27f0e2c5
aphawkins <aph828@gmail.com>
2020-12-24 16:34:32 +0000

    add rot13 razor page

---
+++ libs/Useful.Security.Cryptography.UI/ViewModels/ROT13ViewModel.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="ROT13ViewModel.cs" company="APH Software">
+// Copyright (c) Andrew Hawkins. All rights reserved.
+// </copyright>
+
+namespace Useful.Security.Cryptography.UI.ViewModels
+{
+    /// <summary>
+    /// ViewModel for the ROT13 cipher.
+    /// </summary>
+    public sealed class ROT13ViewModel
+    {
+        private readonly ROT13 _cipher = new();
+
+        /// <summary>
+        /// Gets or sets the encrypted ciphertext.
+        /// </summary>
+        public string Ciphertext { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets the cipher's name.
+        /// </summary>
+        public string CipherName => _cipher.CipherName;
+
+        /// <summary>
+        /// Gets or sets the unencrypted plaintext.
+        /// </summary>
+        public string Plaintext { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Encrypts the plaintext into ciphertext.
+        /// </summary>
+        public void Encrypt() => Ciphertext = _cipher.Encrypt(Plaintext);
+
+        /// <summary>
+        /// Decrypts the ciphertext into plaintext.
+        /// </summary>
+        public void Decrypt() => Plaintext = _cipher.Decrypt(Ciphertext);
+    }
+}
--- test/Useful.Security.Cryptography.UI.Tests/ViewModels/ROT13ViewModelTests.cs
+++ test/Useful.Security.Cryptography.UI.Tests/ViewModels/ROT13ViewModelTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="AtbashViewModelTests.cs" company="APH Software">
+// <copyright file="ROT13ViewModelTests.cs" company="APH Software">
 // Copyright (c) Andrew Hawkins. All rights reserved.
 // </copyright>
 
@@ -7,23 +7,23 @@ namespace Useful.Security.Cryptography.Tests
     using Useful.Security.Cryptography.UI.ViewModels;
     using Xunit;
 
-    public class AtbashViewModelTests
+    public class ROT13ViewModelTests
     {
         [Theory]
-        [InlineData("Hello", "Svool")]
+        [InlineData("Hello", "Uryyb")]
         public void Encrypt(string plaintext, string ciphertext)
         {
-            AtbashViewModel viewmodel = new();
+            ROT13ViewModel viewmodel = new();
             viewmodel.Plaintext = plaintext;
             viewmodel.Encrypt();
             Assert.Equal(ciphertext, viewmodel.Ciphertext);
         }
 
         [Theory]
-        [InlineData("Svool", "Hello")]
+        [InlineData("Uryyb", "Hello")]
         public void Decrypt(string plaintext, string ciphertext)
         {
-            AtbashViewModel viewmodel = new();
+            ROT13ViewModel viewmodel = new();
             viewmodel.Ciphertext = ciphertext;
             viewmodel.Decrypt();
             Assert.Equal(plaintext, viewmodel.Plaintext);
@@ -32,8 +32,8 @@ namespace Useful.Security.Cryptography.Tests
         [Fact]
         public void CipherName()
         {
-            AtbashViewModel viewmodel = new();
-            Assert.Equal("Atbash", viewmodel.CipherName);
+            ROT13ViewModel viewmodel = new();
+            Assert.Equal("ROT13", viewmodel.CipherName);
         }
     }
 }
