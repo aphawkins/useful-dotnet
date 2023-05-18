2eaaa13bbd6a0b7d81f1a7625d550fa111e88178
Andy Hawkins <aph828@gmail.com>
2023-05-18 20:13:55 +0100

    Add colour converter

---
+++ libs/Useful.Drawing/ColourConverter.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="ColourConverter.cs" company="APH Software">
+// Copyright (c) Andrew Hawkins. All rights reserved.
+// </copyright>
+
+namespace Useful.Drawing
+{
+    /// <summary>
+    /// Converts colours from one format to another.
+    /// </summary>
+    public static class ColourConverter
+    {
+        /// <summary>
+        /// Converts 8bit RRRGGGBB to ARGB 32bit colour.
+        /// </summary>
+        /// <param name="color8Bit">The 8bit colour code.</param>
+        /// <returns>The 32bit colour code.</returns>
+        public static int Convert8BitToARGBColor(byte color8Bit)
+        {
+            int red = Math.Clamp((int)Math.Round((((color8Bit >> 5) & 7) * (256f / 7)) - 1), 0, 255);
+            int green = Math.Clamp((int)Math.Round((((color8Bit >> 2) & 7) * (256f / 7)) - 1), 0, 255);
+            int blue = Math.Clamp((int)Math.Round(((color8Bit & 3) * (256f / 3)) - 1), 0, 255);
+
+            return (0xFF << 24) | (red << 16) | (green << 8) | blue;
+        }
+    }
+}
