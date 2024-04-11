c3d191b7161507ee33c026830cb9bd15599eb289
Andy Hawkins <aph828@gmail.com>
2024-04-11 21:03:24 +0100

    improve audio unit tests

--- test/Useful.Audio.Tests/WavFileTests.cs
+++ test/Useful.Audio.Tests/WavFileTests.cs
@@ -1,15 +1,18 @@
 // Copyright (c) Andrew Hawkins. All rights reserved.
 
+using System.Globalization;
+using System.Security.Cryptography;
 using Useful.Audio.Instruments;
 
 namespace Useful.Audio.Tests
 {
-    public class SynthesiserTests
+    public class WavFileTests
     {
         [Fact]
         public void OctaveTests()
         {
             // Arrange
+            string filename = $"{nameof(OctaveTests)}.wav";
             ToneGenerator tone = new();
             Synthesiser synth = new(tone);
 
@@ -23,8 +26,14 @@ namespace Useful.Audio.Tests
             synth.AddNote(new(7, NoteStep.C, TimeSpan.FromSeconds(1)));
             synth.AddNote(new(8, NoteStep.C, TimeSpan.FromSeconds(1)));
 
+            // Act
             WavFile wavFile = new();
-            wavFile.Write($"{nameof(OctaveTests)}.wav", synth);
+            wavFile.Write(filename, synth);
+
+            // Assert
+            Assert.Equal(
+                "155DE8A4CE84F3F27714C9C892DAC70A407B8C419798E7891A4C55D3C69FCC57",
+                string.Concat(SHA256.HashData(File.ReadAllBytes(filename)).Select(x => x.ToString("X2", CultureInfo.InvariantCulture))));
         }
     }
 }
